a347fec11cb0b6e5508bd3ebd95d8fbc4e17eeee
Areeba <turabi.areeba@gmail.com>
2018-04-20 11:17:45 -0700

    cleaned up

--- ZoneMappingGUI.java
+++ ZoneMappingGUI.java
@@ -19,27 +19,10 @@ public class ZoneMappingGUI implements ActionListener{
 
     //GUI components
     JFrame frame;
-    JMenuBar menuBar;
-    JMenu fileM;
-    JMenuItem openMI;
-    JComboBox numWordsCB;
-    JComboBox langCB;
     JButton goB;
     JButton showB;
     JPanel topP;
     JPanel centerP;
-    JPanel zone1;
-    JPanel zone2;
-    JPanel zone3;
-    JPanel zone4;
-    JPanel zone5;
-    JPanel zone6;
-    JPanel zone7;
-    JPanel zone8;
-    JPanel zone9;
-    JPanel zone10;
-    JPanel zone11;
-    JPanel zone12;
     JPanel wordP;
     JPanel buttonP;
     JPanel meaningP;
@@ -56,7 +39,6 @@ public class ZoneMappingGUI implements ActionListener{
 
     String fileName;
 
-    //Translations translations;
 
     //Create the GUI for our program
     @SuppressWarnings("unchecked")
@@ -83,71 +65,27 @@ public class ZoneMappingGUI implements ActionListener{
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //Set up menu bar
-        /*Font f = new Font("sans-serif", Font.PLAIN, FONT_SIZE2);
-        UIManager.put("Menu.font", topFont);
-        UIManager.put("MenuItem.font", topFont);
-        menuBar = new JMenuBar();
-        fileM = new JMenu("File");
-        openMI = new JMenuItem("Open");
-        openMI.addActionListener(this);
-        fileM.add(openMI);
-        menuBar.add(fileM);
-        frame.setJMenuBar(menuBar);*/
-
         //Main JPanel
         JPanel mainP = new JPanel(); //Use default border layout
         mainP.setBackground(panelColor);
 
-        //Set up top panel with combo boxes and Go button
+        //Set up top panel with button
         topP = new JPanel();
         topP.setPreferredSize(new Dimension(TOP_WIDTH, TOP_HEIGHT));
         topP.setLayout(new GridLayout(1, 3, 10, 0)); //1x3 with 10px hgap
 
-        JPanel langP = new JPanel();
-        langP.setBorder(new TitledBorder("Select language"));
-        ((javax.swing.border.TitledBorder)langP.getBorder()).setTitleFont(topFontSml);
-        langCB = new JComboBox();
-        langCB.setFont(topFont);
-        langCB.insertItemAt("Show English", 0);
-        langCB.insertItemAt("Show Chinese", 1);
-        langCB.setEnabled(false); //Don't enable until file chosen
-        //langCB.addActionListener(this);
-        langP.add(langCB);
-
-        JPanel numWordsP = new JPanel();
-        numWordsP.setBorder(new TitledBorder("Number of words to display"));
-        ((javax.swing.border.TitledBorder)numWordsP.getBorder()).setTitleFont(topFontSml);
-        numWordsCB = new JComboBox();
-        numWordsCB.setFont(topFont);
-        numWordsCB.setEnabled(false);
-        //numWordsCB.addActionListener(this);
-        numWordsP.add(numWordsCB);
-
-        JPanel goP = new JPanel();
-        goP.setLayout(new BorderLayout());
-        goP.setBorder(new EmptyBorder(10, 10, 10, 10));
-        goB = new JButton("Please choose a file first");
-        goB.setFont(topFont);
-        goB.setEnabled(false);
-        goB.addActionListener(this);
-        goP.add(goB);
-
-        topP.add(langP);
-        topP.add(numWordsP);
-        topP.add(goP);
-
-        topP.removeAll();
-        goB = new JButton("im a booton");
+        //add button to top panel
+        goB = new JButton("im a booton press me to highlight zone 9");
         goB.addActionListener(this);
         topP.add(goB);
 
-        //Set up structure of center panel for text and buttons
+        //Set up structure of center panel for zone map (3x4)
         centerP = new JPanel();
         centerP.setPreferredSize(new Dimension(CENTER_WIDTH, CENTER_HEIGHT));
-        centerP.setLayout(new GridLayout(3, 4, 10, 10)); //1x2 with 10px hgap
+        centerP.setLayout(new GridLayout(3, 4, 10, 10)); //3x4 with 10px hgap 10px vgap
         centerP.setBackground(panelColor);
 
+        //make zone labels with 0th (zone 12) label highlighted
         makeZoneLabels(0);
 
         mainP.add(topP, BorderLayout.PAGE_START);
@@ -158,7 +96,9 @@ public class ZoneMappingGUI implements ActionListener{
 
     }
 
+    //Fill zoneLabels array with Zone strings,
     private void makeZoneLabels(int currZone){
+        //Fill zoneLabels array with Zone strings
         zoneLabels = new JLabel[12];
         for(int i = 0; i < 12; i++){
             int zoneNum;
@@ -175,7 +115,7 @@ public class ZoneMappingGUI implements ActionListener{
             zoneLabels[i].setFont(new Font("Verdana",1,20));
 
         }
-
+        //Fill zones array with zone JPanels containing zoneLabels strings
         zones = new JPanel[12];
         for(int i = 0; i < 12; i++){
             zones[i] = new JPanel();
@@ -189,98 +129,13 @@ public class ZoneMappingGUI implements ActionListener{
         }
     }
 
-    //Dynamically add a button onto the GUI
-    //On click, reveal associated label
-    private void addButton(JLabel label){
-        JButton j = new JButton("View translation");
-        j.setFont(font);
-        j.addActionListener(new ActionListener(){
-                public void actionPerformed(ActionEvent e){
-                    j.setVisible(false);
-                    label.setVisible(true);
-                }
-            });
-        buttonP.add(j);
-    }
-
-    private void addText(int num, String[] words, int lang){
-        for(int i = 0; i < num; i++){
-            //Take advantage of english = 0, chinese = 1 for lang
-            //This just avoids a few if-statements
-            JLabel wordL = new JLabel(words[2*i + lang]);
-            wordL.setFont(font);
-            JLabel meaningL = new JLabel(words[2*i + (lang+1)%2]);
-            meaningL.setFont(font);
-            wordL.setHorizontalAlignment(JLabel.CENTER);
-            meaningL.setHorizontalAlignment(JLabel.CENTER);
-            meaningL.setVisible(false);
-
-            addButton(meaningL);
-            wordP.add(wordL);
-            meaningP.add(meaningL);
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    private void enableInput(){
-        langCB.setEnabled(true);
-        langCB.setSelectedIndex(0);
-        numWordsCB.setEnabled(true);
-        //Add options for num words, 10 max
-        for(int i = 1; i <= 10 && i <= 0;++i){
-            numWordsCB.insertItemAt(i, i-1);
-        }
-        numWordsCB.setSelectedIndex(0);
-        goB.setEnabled(true);
-        goB.setText("Go!");
-        frame.setTitle("Learn Chinese! Reading from " + fileName);
-    }
-
-    private void disableInput(){
-        langCB.setEnabled(false);
-        numWordsCB.setEnabled(false);
-        goB.setEnabled(false);
-        goB.setText("Please choose a file first");
-        frame.setTitle("Learn Chinese!  No file loaded");
-    }
 
     public void actionPerformed(ActionEvent e){
-        /*if(e.getSource() == openMI){
-            translations = new Translations();
-            int approve = fc.showOpenDialog(null);
-            if(approve == JFileChooser.APPROVE_OPTION){
-                fileName = fc.getSelectedFile().getName();
-                try{
-                    translations.readFile(fileName);
-                    enableInput();
-                } catch(Exception ioex){
-                    translations = null;
-                    disableInput();
-                    JOptionPane.showMessageDialog(null,"Error reading file\n" + ioex.getMessage(),"Error",JOptionPane.ERROR_MESSAGE);
-                } finally {
-                    topP.revalidate();
-                    topP.repaint();
-                }
-            }
-        } else if(e.getSource() == goB){*/
-            //int numWords = numWordsCB.getSelectedIndex() + 1;
-            //int lang = langCB.getSelectedIndex();
-            //String[] words = translations.getRandomWords(numWords);
-
-            //Remove old components
-            //wordP.removeAll();
-            //buttonP.removeAll();
-            //meaningP.removeAll();
-
-
-            //addText(numWords, words, lang);
-
             centerP.removeAll();
             makeZoneLabels(1);
 
             //Force components to repaint
             centerP.revalidate();
             centerP.repaint();
-        //}
     }
 }
